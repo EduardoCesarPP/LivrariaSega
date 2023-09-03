61715873309e87c24e4fa075c72103e3aa515f1c
Eduardo Cesar <64036535+EduardoCesarPP@users.noreply.github.com>
2023-09-03 01:28:04 -0300

    Add files via upload

---
+++ ProjetoLivraria/Sistema/Login.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ProjetoLivraria.Sistema
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pbMostra = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMostra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.Color.DarkGray;
+            this.txtUsuario.Location = new System.Drawing.Point(21, 98);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(326, 32);
+            this.txtUsuario.TabIndex = 7;
+            this.txtUsuario.Text = " Login";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtLogin_Enter);
+            this.txtUsuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PressEnter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtLogin_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(201)))), ((int)(((byte)(14)))));
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 43);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Livraria Sega";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.ForeColor = System.Drawing.Color.DarkGray;
+            this.txtSenha.Location = new System.Drawing.Point(21, 136);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(285, 32);
+            this.txtSenha.TabIndex = 8;
+            this.txtSenha.Text = " Senha";
+            this.txtSenha.Enter += new System.EventHandler(this.txtSenha_Enter);
+            this.txtSenha.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PressEnter);
+            this.txtSenha.Leave += new System.EventHandler(this.txtSenha_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(178)))), ((int)(((byte)(10)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 50);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Logar);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(178)))), ((int)(((byte)(10)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(335, 35);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Cancelar);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 33);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(12, 136);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 33);
+            this.panel2.TabIndex = 14;
+            // 
+            // pbMostra
+            // 
+            this.pbMostra.Image = global::ProjetoLivraria.Properties.Resources.btnEsconderSenha;
+            this.pbMostra.Location = new System.Drawing.Point(312, 136);
+            this.pbMostra.Name = "pbMostra";
+            this.pbMostra.Size = new System.Drawing.Size(35, 32);
+            this.pbMostra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMostra.TabIndex = 12;
+            this.pbMostra.TabStop = false;
+            this.pbMostra.Click += new System.EventHandler(this.MostarSenha);
+            this.pbMostra.MouseEnter += new System.EventHandler(this.Aumentar);
+            this.pbMostra.MouseLeave += new System.EventHandler(this.Diminuir);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(201)))), ((int)(((byte)(14)))));
+            this.ClientSize = new System.Drawing.Size(359, 272);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pbMostra);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMostra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pbMostra;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ ProjetoLivraria/Sistema/Login.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoLivraria.Sistema
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            //this.BackColor = Color.BurlyWood;
+            //TransparencyKey = Color.BurlyWood;
+            InitializeComponent();
+        }
+
+
+        public static int Codigo;
+        public static string Nome;
+        public static string Funcao;
+
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            var botao = (PictureBox)sender;
+            int h = botao.Size.Height, w = botao.Size.Width, x = botao.Location.X, y = botao.Location.Y;
+            botao.Size = new Size(w + 2, h + 2);
+            botao.Location = new Point(x - 1, y - 1);
+        }
+
+        private void Diminuir(object sender, EventArgs e)
+        {
+            var botao = (PictureBox)sender;
+            int h = botao.Size.Height, w = botao.Size.Width, x = botao.Location.X, y = botao.Location.Y;
+            botao.Size = new Size(w - 2, h - 2);
+            botao.Location = new Point(x + 1, y + 1);
+        }
+
+        private void MostarSenha(object sender, EventArgs e)
+        {
+            if (this.txtSenha.PasswordChar == '*')
+            {
+                pbMostra.Image = ProjetoLivraria.Properties.Resources.btnMostrarSenha2;
+                if (this.txtSenha.ForeColor != Color.Gray)
+                {
+                    this.txtSenha.PasswordChar = '\0';
+                }
+            }
+            else
+            {
+                pbMostra.Image = ProjetoLivraria.Properties.Resources.btnEsconderSenha;
+                if (this.txtSenha.ForeColor != Color.Gray)
+                {
+                    this.txtSenha.PasswordChar = '*';
+                }
+            }
+        }
+
+        private void txtLogin_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == " Login")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtLogin_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = " Login";
+                txtUsuario.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtSenha_Enter(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == " Senha")
+            {
+                txtSenha.Text = "";
+                txtSenha.ForeColor = Color.Black;
+                txtSenha.PasswordChar = '*';
+            }
+        }
+
+        private void txtSenha_Leave(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == "")
+            {
+                if (pbMostra.Image == ProjetoLivraria.Properties.Resources.btnEsconderSenha)
+                {
+                    txtSenha.Text = " Senha";
+                    txtSenha.ForeColor = Color.Gray;
+                    txtSenha.PasswordChar = '\0';
+                }
+            }
+        }
+
+        private void Logar(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text != "" && txtUsuario.Text != " Login" && txtSenha.Text != "" && txtSenha.Text != " Senha")
+            {
+                BLL.Login l = new BLL.Login();
+                l.Usuario = txtUsuario.Text;
+                l.Senha = txtSenha.Text;
+                if (l.Logar())
+                {
+                    Codigo = l.Codigo;
+                    Nome = l.Nome;
+                    Funcao = l.Funcao;
+                    if (txtSenha.Text == "123456")
+                    {
+                        string NovaSenha;
+                        NovaSenha = Interaction.InputBox("Identificamos que este é seu primeiro acesso.\nPor favor insira sua nova senha.", "Nova senha", "", 650, 300);
+                        if (NovaSenha == "")
+                        {
+                            txtUsuario.Text = "";
+                            txtSenha.Text = "";
+                            return;
+                        }
+                        while (NovaSenha == txtUsuario.Text || NovaSenha == "123456")
+                        {
+                            if (NovaSenha == "")
+                            {
+                                txtUsuario.Text = "";
+                                txtSenha.Text = "";
+                                return;
+                            }
+
+                            NovaSenha = Interaction.InputBox("A senha deve ser diferente do usuário e de 123456.\nPor favor insira sua nova senha.", "Nova senha", "", -1, -1);
+                        }
+
+
+                        l.AlterarSenha(NovaSenha);
+                        Negocio.Livros.LivrosMenu li = new Negocio.Livros.LivrosMenu();
+                        this.Hide();
+                        li.ShowDialog();
+
+                    }
+                    else
+                    {
+                        Negocio.Livros.LivrosMenu li = new Negocio.Livros.LivrosMenu();
+                        this.Hide();
+                        li.ShowDialog();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Erro Usuário/Senha");
+                }
+            }
+        }
+
+        private void Cancelar(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PressEnter(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (txtSenha.Text.Length == 0)
+                {
+                    txtSenha.Focus();
+                }
+                else
+                {
+                    EventArgs ee = new EventArgs();
+                    Logar(sender, ee);
+                }
+            }
+        }
+    }
+}
